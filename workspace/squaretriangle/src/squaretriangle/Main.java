c356385bd9983dfebc9c391e60f6dc36f8fb7018
daniel <drkspace@gmail.com>
2016-07-13 10:58:33 -0400

    1st go

---
+++ workspace/squaretriangle/src/squaretriangle/Main.java
@@ -0,0 +1,25 @@
+package squaretriangle;
+//Made by drkspace
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		double i = 0;
+		while(true){
+			i++;
+			double squared = i*i;
+			for(double k = 0; k<10000000; k++){
+				double triangle= ((k+1)*k)/2;
+				if(triangle>squared) break;
+				if(triangle==squared) {
+					System.out.println(squared+": "+i+": "+k);
+					break;
+				}
+			}
+
+		}
+
+	}
+
+}
