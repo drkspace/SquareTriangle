bb9fb706c16d92945e03176b2fa027f56c41a62c
daniel <drkspace@gmail.com>
2016-07-13 14:24:35 -0400

    made k start at the previous triangle number

--- workspace/squaretriangle/src/squaretriangle/Main.java
+++ workspace/squaretriangle/src/squaretriangle/Main.java
@@ -4,16 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
+		double previous=0;
 		double i = 0;
+		
 		while(true){
 			i++;
 			double squared = i*i;
-			for(double k = 0; k<squared; k++){
+
+			for(double k = previous; k<squared; k++){
 				double triangle= ((k+1)*k)/2;
+				
 				if(triangle>squared) break;
+				
 				if(triangle==squared) {
 					System.out.println(squared+": "+i+": "+k);
+					previous=k;
 					break;
 				}
 			}
